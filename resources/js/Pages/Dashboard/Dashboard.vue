a1787a5a2ff6beb748c143d26597e513bfa4ebac
Davi <davicampos29@hotmail.com>
2024-06-21 16:04:23 -0300

    adicionando cards de departamentos ao dashboard

---
+++ resources/js/Pages/Dashboard/Dashboard.vue
@@ -0,0 +1,28 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import TasksListing from './Partials/TasksListing.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-4">
+
+                    <!-- listagem home -->
+                    <TasksListing />
+
+
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
---
+++ resources/js/Pages/Dashboard/Partials/TasksListing.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <h1 class="text-center text-gray-500 text-3xl mb-8">Departamentos</h1>
+    <div class="w-full flex flex-col md:flex-row gap-4 justify-center">
+
+        <div v-for="i in 5"
+            class="w-100 md:w-1/6 h-auto shadow-md py-4 border rounded-md flex flex-col justify-center items-center ">
+            <h1 class="text-2xl text-gray-500 my-2">Nome categoria</h1>
+
+            <div class="bg-gray-200 rounded-full w-40 h-40"></div>
+
+            <p class="my-2">1/2 concluidos</p>
+
+
+        </div>
+
+    </div>
+
+
+</template>
