ed48797c3352c06d190db057c916c13e66260d24
Davi <davicampos29@hotmail.com>
2024-06-21 16:22:33 -0300

    icones

--- resources/js/Pages/Dashboard/Partials/TasksListing.vue
+++ resources/js/Pages/Dashboard/Partials/TasksListing.vue
@@ -1,5 +1,6 @@
 <script setup>
 
+import { BeakerIcon, CurrencyDollarIcon, UserGroupIcon, CodeBracketIcon, BanknotesIcon, ChartBarSquareIcon } from '@heroicons/vue/24/outline'
 </script>
 
 <template>
@@ -7,11 +8,63 @@
     <h1 class="text-center text-gray-500 text-3xl mb-8">Departamentos</h1>
     <div class="w-full flex flex-col md:flex-row gap-4 justify-center">
 
-        <div v-for="i in 5"
-            class="w-100 md:w-1/6 h-auto shadow-md py-4 border rounded-md flex flex-col justify-center items-center ">
-            <h1 class="text-2xl text-gray-500 my-2">Nome categoria</h1>
+        <div class="w-100 md:w-1/6 h-auto shadow-md py-4 border rounded-md flex flex-col justify-center items-center ">
+            <h1 class="text-xl text-gray-500 my-2 font-bold">Financeiro</h1>
 
-            <div class="bg-gray-200 rounded-full w-40 h-40"></div>
+            <div class="bg-green-100 rounded-full w-40 h-40 flex justify-center items-center">
+                <CurrencyDollarIcon class="w-16 text-green-500 " />
+            </div>
+
+            <p class="my-2">1/2 concluidos</p>
+
+
+        </div>
+
+        <div class="w-100 md:w-1/6 h-auto shadow-md py-4 border rounded-md flex flex-col justify-center items-center ">
+            <h1 class="text-xl text-gray-500 my-2 font-bold">Desenvolvimento</h1>
+
+            <div class="bg-indigo-100 rounded-full w-40 h-40 flex justify-center items-center">
+                <CodeBracketIcon class="w-16 text-indigo-500 " />
+            </div>
+
+            <p class="my-2">1/2 concluidos</p>
+
+
+        </div>
+
+
+        <div class="w-100 md:w-1/6 h-auto shadow-md py-4 border rounded-md flex flex-col justify-center items-center ">
+            <h1 class="text-xl text-gray-500 my-2 font-bold">Vendas</h1>
+
+            <div class="bg-yellow-100 rounded-full w-40 h-40 flex justify-center items-center">
+                <BanknotesIcon class="w-16 text-yellow-500 " />
+            </div>
+
+            <p class="my-2">1/2 concluidos</p>
+
+
+        </div>
+
+
+        <div class="w-100 md:w-1/6 h-auto shadow-md py-4 border rounded-md flex flex-col justify-center items-center ">
+            <h1 class="text-xl text-gray-500 my-2 font-bold">Marketing</h1>
+
+            <div class="bg-blue-100 rounded-full w-40 h-40 flex justify-center items-center">
+                <ChartBarSquareIcon class="w-16 text-blue-500 " />
+            </div>
+
+            <p class="my-2">1/2 concluidos</p>
+
+
+        </div>
+
+
+        <div class="w-100 md:w-1/6 h-auto shadow-md py-4 border rounded-md flex flex-col justify-center items-center ">
+            <h1 class="text-md text-gray-500 my-2 text-center leading-5 font-bold">Atendimento <br> ao cliente</h1>
+
+            <div class="bg-orange-100 rounded-full w-40 h-40 flex justify-center items-center">
+                <UserGroupIcon class="w-16 text-orange-500 " />
+            </div>
 
             <p class="my-2">1/2 concluidos</p>
 
