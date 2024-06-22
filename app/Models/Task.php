ba21ae94df862c0d320025730dda39ea45f59dbb
Davi <davicampos29@hotmail.com>
2024-06-22 20:40:01 -0300

    relacionamento de entidades

---
+++ app/Models/Task.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'start_date',
+        'is_finished',
+        'user_id',
+        'project_id'
+    ];
+
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+}
